f14c47104f57df1dc42763714562fc3c317c1f9b
Adesh <adeshramchurn@googlemail.com>
2023-01-09 19:33:05 +0000

    added first half of sign in screen

--- backend/data.js
+++ backend/data.js
@@ -1,64 +1,80 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-    products: [
+
+    users: [
 
         {
-            _id: '1',
-            name: 'Nike Slim shirt',
-            slug: 'nike-slim-shirt', //what you will see in the url
-            category: 'Shirts',
-            image: '/images/p1.jpg',
-            price: 120,
-            countInStock: 0,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality shirt',
+            name: 'Adesh',
+            email: 'test@email.com',
+            password: bcrypt.hashSync('Testing123'),
+            isAdmin: true,
         },
 
         {
-            _id: '2',
-            name: 'Nike Slim Pants',
-            slug: 'nike-slim-pants', //what you will see in the url
-            category: 'Pants',
-            image: '/images/p2.jpg',
-            price: 12,
-            countInStock: 10,
-            brand: 'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality pants',
-        },
+            name: 'user1',
+            email: 'user1@email.com',
+            password: bcrypt.hashSync('Testing123'),
+            isAdmin: false,
 
-        {
-            _id: '3',
-            name: 'Adidas Fit Pant',
-            slug: 'adidas-fit-pant',
-            category: 'Pants',
-            image: '/images/p3.jpg',
-            price: 200,
-            countInStock: 10,
-            brand: 'Adidas',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality pants',
-        },
-
-        {
-            _id: '4',
-            name: 'Adidas Fit Shirt',
-            slug: 'adidas-fit-shirt',
-            category: 'Shirts',
-            image: '/images/p4.jpg',
-            price: 150,
-            countInStock: 10,
-            brand: 'Adidas',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality pants',
-        },
+        }
 
 
     ],
-};
-
-export default data;
+    products: [
+      {
+        // _id: '1',
+        name: 'Nike Slim shirt',
+        slug: 'nike-slim-shirt',
+        category: 'Shirts',
+        image: '/images/p1.jpg', // 679px × 829px
+        price: 120,
+        countInStock: 10,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      },
+      {
+        // _id: '2',
+        name: 'Adidas Fit Shirt',
+        slug: 'adidas-fit-shirt',
+        category: 'Shirts',
+        image: '/images/p2.jpg',
+        price: 250,
+        countInStock: 0,
+        brand: 'Adidas',
+        rating: 4.0,
+        numReviews: 10,
+        description: 'high quality product',
+      },
+      {
+        // _id: '3',
+        name: 'Nike Slim Pant',
+        slug: 'nike-slim-pant',
+        category: 'Pants',
+        image: '/images/p3.jpg',
+        price: 25,
+        countInStock: 15,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product',
+      },
+      {
+        // _id: '4',
+        name: 'Adidas Fit Pant',
+        slug: 'adidas-fit-pant',
+        category: 'Pants',
+        image: '/images/p4.jpg',
+        price: 65,
+        countInStock: 5,
+        brand: 'Puma',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product',
+      },
+    ],
+  };
+  export default data;
+  
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,55 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+dotenv.config(); // to fectch variable from .env file
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('connected to db')
+}).catch(err => {
+    console.log(err.message);
+})
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message});
+});
 // test
-app.get('/api/products', (req, res) => {
+// app.get('/api/products', (req, res) => {
 
-    res.send(data.products)
-});
+//     res.send(data.products)
+// });
 
-app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug);
+// app.get('/api/products/slug/:slug', (req, res) => {
+//     const product = data.products.find(x => x.slug === req.params.slug);
 
-    if(product) {
-        res.send(product);
-    } else {
-        res.status(404).send({ message: 'Product Not Found'});
-    }
-});
+//     if(product) {
+//         res.send(product);
+//     } else {
+//         res.status(404).send({ message: 'Product Not Found'});
+//     }
+// });
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find(x => x._id === req.params.id);
+// app.get('/api/products/:id', (req, res) => {
+//     const product = data.products.find(x => x._id === req.params.id);
 
-    if(product) {
-        res.send(product);
-    } else {
-        res.status(404).send({ message: 'Product Not Found'});
-    }
-});
+//     if(product) {
+//         res.send(product);
+//     } else {
+//         res.status(404).send({ message: 'Product Not Found'});
+//     }
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
