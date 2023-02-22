781f26ff81c7230867adc95be1b4acab7ff809f2
Adesh <adeshramchurn@googlemail.com>
2023-02-22 22:03:33 +0000

    added delete user functionality for admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -51,6 +51,28 @@ userRouter.put(
   })
 );
 
+
+userRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if(user) {
+      if(user.email === 'test@email.com') {
+        res.status(400).send({ message: 'Can Not Delete Admin User'});
+        return;
+      }
+
+      await user.remove();
+      res.send({ message: 'User Deleted' });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
 userRouter.post(
   '/signin',
   expressAsyncHandler(async (req, res) => {
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -7,6 +7,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
+import { toast } from 'react-toastify';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -20,7 +21,14 @@ const reducer = (state, action) => {
             };
         case 'FETCH_FAIL':
             return {...state, loading: false, error: action.payload };
-
+        case 'DELETE_REQUEST':
+            return {...state, loadingDelete: true, successDelete: false };
+        case 'DELETE_SUCCESS':
+            return {...state, loadingDelete: false, successDelete: true };
+        case 'DELETE_FAIL':
+            return {...state, loadingDelete: false };
+        case 'DELETE_RESET':
+            return {...state, loadingDelete: false, successDelete: false };
         default:
             return state;
     }
@@ -30,7 +38,7 @@ export default function UserListScreen() {
 
     const navigate = useNavigate();
 
-    const [{ loading, error, users }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, users, loadingDelete, successDelete }, dispatch] = useReducer(reducer, {
         loading: true,
         error: '',
     });
@@ -62,9 +70,38 @@ export default function UserListScreen() {
     
         };
 
-        fetchData();
+        if(successDelete) {
+            dispatch({type: 'DELETE_RESET' });
+        } else {
+            fetchData();
 
-    }, [userInfo]);
+        }
+
+    }, [userInfo, successDelete]);
+
+    const deleteHandler = async (user) => {
+
+        if (window.confirm('Are you sure to delete')) {
+
+            try {
+                dispatch({ type: 'DELETE_REQUEST' });
+                await axios.delete(`/api/users/${user._id}`, {
+                    headers: { Authorization: `Bearer ${userInfo.token}` }
+                });
+                toast.success('User deleted successfully');
+                dispatch({ type: 'DELETE_SUCCESS' });
+                
+            } catch (error) {
+                toast.error(getError(error));
+                dispatch({
+                    type: 'DELETE_FAIL',
+                });
+                
+            }
+
+        }
+
+    }
 
 
   return (
@@ -74,7 +111,7 @@ export default function UserListScreen() {
             <title>Users</title>
         </Helmet>
         <h1>Users</h1>
-
+        {loadingDelete && <LoadingBox></LoadingBox>}
         {loading ? (
             <LoadingBox></LoadingBox>
         ): error ? (
@@ -106,6 +143,15 @@ export default function UserListScreen() {
                                 >
                                     Edit
                                 </Button>
+                                &nbsp;
+                                <Button
+                                    type="button"
+                                    variant="light"
+                                    onClick={() => deleteHandler(user)}
+                                >
+                                    Delete
+                                </Button>
+
                             </td>
                         </tr>
 
